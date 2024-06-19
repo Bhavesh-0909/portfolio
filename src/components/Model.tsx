6b6a23f74cb1c57ca57f7cfb0e59f796e703d414
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2024-06-19 16:43:03 +0530

    again a type error

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -11,14 +11,16 @@ interface ModelProps {
 
 function Model({isVisible, data, onClose}:ModelProps) {
     if (!isVisible) return null;
-    function handelClose(e){
-        if (e.target.id === 'wrapper') onClose();
+    function handleClose(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+      const target = e.target as HTMLElement;
+    if (target.id === 'wrapper') onClose();
     }
+  
 
   return (
     <div 
     id='wrapper'
-    onClick={handelClose}
+    onClick={handleClose}
     className='w-screen h-screen p-5 fixed inset-0 bg-black/25 backdrop-blur-sm flex justify-center items-center'>
         <div 
         className='md:w-[600px] w-full bg-white'>
