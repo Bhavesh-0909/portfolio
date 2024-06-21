e11c7115da5743040d44ba1342330ba6603b2c55
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2024-06-21 13:25:33 +0530

    changes in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { MdDarkMode } from "react-icons/md";
 import { IoMdMenu } from "react-icons/io";
 import logo from "../assets/bc.png";
@@ -9,57 +9,65 @@ import { usePathname } from 'next/navigation';
 
 function Navbar() {
   const pathname = usePathname();
+  const [menu, setMenu] = useState<boolean>(false);
+
   return (
-    <div className='w-screen py-4 px-10 flex justify-between md:space-x-40 items-center bg-[#0E0E10]/85 fixed z-[100] top-0'>
-        <div className='w-8 h-8 rounded-full overflow-hidden flex items-center justify-center'>
-          <Link href={'/'}>
-            <Image src={logo} alt="logo" className='w-8 h-8' />
-          </Link>
+    <div 
+    className='w-screen h-fit py-4 px-10 flex items-center justify-center bg-[#0E0E10]/85 fixed z-[100] top-0'>
+      
+      <div
+      className='max-w-[1080px] mx-auto w-full flex justify-between items-center md:space-x-40 '
+      >
+        <div className='md:hidden flex items-center justify-center w-10 h-10 right-10 top-5 z-50'>
+            {menu ? 
+              (<button onClick={()=>setMenu(!menu)} className='h-full w-full font-bold text-lg'>X</button>):
+              (<button onClick={()=>setMenu(!menu)} ><IoMdMenu className='w-6 h-6'/></button>)}
         </div>
 
-        <div className='hidden md:block'>
-          <div className='flex justify-center items-center space-x-6'>
-
-            <Link href={"/"}>
-              <div 
-              className={pathname === '/' ? 'px-2 py-1 rounded-md bg-gray-900/95': ""}>
-                About
-              </div>
-            </Link>
-
-            <Link href={"/projects"}>
-              <div 
-              className={pathname === '/projects' ? 'px-2 py-1 rounded-md bg-gray-900/95': ""}>
-                Projects
-              </div>
-            </Link>
-
-            <Link href={"/blog"}>
-              <div 
-              className={pathname === '/blog' ? 'px-2 py-1 rounded-md bg-gray-900/95': ""}>
-                Blogs
-              </div>
-            </Link>
-
-            <Link href={"/contact"}>
-              <div 
-              className={pathname === '/contact' ? 'px-2 py-1 rounded-md bg-gray-900/95': ""}>
-                Contact
-              </div>
-            </Link>
-          </div>
-        </div>
+        <Link href={'/'}>
+          <Image src={logo} alt="logo" className='w-8 h-8 rounded-full' />
+        </Link>
         
-      <div className='flex justify-between gap-2 items-center'>
+        <div className={`flex md:max-w-[50%] text-center md:h-full md:flex-row md:justify-center mx-auto items-center md:space-x-6  flex-col
+              md:static md:z-auto fixed md:bg-transparent bg-black 
+              ${menu ? "top-0 left-[50%] right-[50%] z-30 w-[80vw] h-screen" : "-right-[50%]"}
+              transition duration-[10000ms] ease-in-out `}>
+
+              <Link href={"/"}>
+                <div 
+                className={pathname === '/' ? 'px-2 py-1 text-center rounded-md bg-gray-900/95': ""}>
+                  About
+                </div>
+              </Link>
+
+              <Link href={"/projects"}>
+                <div 
+                className={pathname === '/projects' ? 'px-2 py-1 rounded-md bg-gray-900/95': ""}>
+                  Projects
+                </div>
+              </Link>
+
+              <Link href={"/blog"}>
+                <div 
+                className={pathname === '/blog' ? 'px-2 py-1 rounded-md bg-gray-900/95': ""}>
+                  Blogs
+                </div>
+              </Link>
+
+              <Link href={"/contact"}>
+                <div 
+                className={pathname === '/contact' ? 'px-2 py-1 rounded-md bg-gray-900/95': ""}>
+                  Contact
+                </div>
+              </Link>
+        </div>
+          
         <div>
-            <MdDarkMode className='w-7 h-7' width={150} height={150}/>
+              <MdDarkMode className='w-7 h-7' width={150} height={150}/>
         </div>
 
-        <div className='md:hidden'>
-          <IoMdMenu className='w-8 h-8 text-white' width={150} height={150}/>
-        </div>
       </div>
-        
+
     </div>
   )
 }
