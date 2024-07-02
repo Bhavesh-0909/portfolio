00b2251116f3feafa205345e2a5f96bf7716dab0
Bhavesh Choudhary <bhaveshchoudhary0909@gmail.com>
2024-07-02 16:39:12 +0530

    contact page completed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,62 +4,85 @@ import React from 'react'
 import { FaLocationDot } from "react-icons/fa6"
 import { IoIosCall } from "react-icons/io";
 import { MdEmail } from "react-icons/md";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
 
 function Page() {
   const {menu, setMenu} = MenuStore()
   return (
     <div
-    className='w-screen max-h-screen h-auto bg-black mt-10  bg-grid-small-white/[0.1] relative flex items-center justify-center'
+    className='w-screen min-h-screen h-auto bg-black  bg-grid-small-white/[0.19] py-10 relative flex items-center justify-center'
     onClick={()=> setMenu(false)}
     >
       <div
       className={menu ? "bg-black/10 backdrop-blur-sm w-screen h-full absolute inset-0 z-[90] overscroll-none" : "" } 
       ></div>
+      
+
       <div 
-      className='bg-black border border-white/50 rounded-md max-w-[800px] w-full min-h-[80vh] h-full mt-8 flex items-center'
-      >
-        <div className='min-w-[49%] px-6 py-6 flex flex-col gap-16'>
+      className='bg-black border relative border-white/50 rounded-md md:mt-14 mt-24  flex flex-col md:flex-row md:w-[50%] md:h-[360px] justify-between'
+      > 
+      <h1 className='text-5xl font-bold absolute -top-12'>Contact Me</h1>
+        <div className='w-full px-6 py-6 flex flex-col items-center justify-evenly'>
 
-          <h1 className='text-6xl font-bold'>Contact Me</h1>
-
+          
           <div className='flex flex-col gap-6 items-center'>
-            <div className='flex w-full items-center gap-8'>
-              <FaLocationDot className='w-8 h-8'/>
+            <div className='flex w-full items-center gap-6'>
+              <FaLocationDot className='w-7 h-7'/>
               <div>
-                <div className='text-xl font-semibold'>Address</div>
-                <div className='text-sm text-white/50 '>Navi Mumbai</div>
+                <div className='text-lg font-semibold'>Address</div>
+                <div className='text-xs text-white/50 '>Navi Mumbai</div>
               </div>
             </div>
 
-            <div className='flex w-full items-center gap-8'>
-              <MdEmail className='w-8 h-8'/>
+            <div className='flex w-full items-center gap-6'>
+              <MdEmail className='w-7 h-7'/>
               <div>
-                <div className='text-xl font-semibold'>Email</div>
+                <div className='text-lg font-semibold'>Email</div>
                 <div className='text-xs text-white/50 '>bhaveshchoudhary0909@gmail.com</div>
               </div>
             </div>
 
-            <div className='flex w-full items-center gap-8'>
-              <IoIosCall className='w-8 h-8'/>
+            <div className='flex w-full items-center gap-6'>
+              <IoIosCall className='w-7 h-7'/>
               <div>
-                <div className='text-xl font-semibold'>Contact</div>
-                <div className='text-sm text-white/50 '>+91 9867502676</div>
+                <div className='text-lg font-semibold'>Contact</div>
+                <div className='text-xs text-white/50 '>+91 9867502676</div>
               </div>
             </div>
           </div>
 
         </div>
 
-        <div
-        className='h-full w-[1px] bg-white '
-        >
-        </div>
+        <div className='w-full px-6 py-6 flex items-center justify-evenly '>
+          <div className='w-full h-full flex flex-col items-center justify-evenly'>
+            <div className='w-full '>
+              <Label htmlFor="name">Your Name</Label>
+              <Input id="name" placeholder="Bhavesh" type="text" />
+            </div>
+            
+            <div className='w-full '>
+              <Label htmlFor="firstname">Your Email</Label>
+              <Input id="Name" placeholder="sample@gmail.com" type="text" />
+            </div>
 
-        <div className='min-w-[49%] px-6 py-6'>
-          email
+            <div className='h-[50%] w-full '>
+              <Label htmlFor="firstname">Message</Label>
+              <Input className='h-[100px] flex' id="Name" placeholder="Message" type="text" />
+            </div>
+            <HoverBorderGradient
+            containerClassName="rounded-full w-full"
+            as="button"
+            className=" font-semibold text-black dark:text-white flex items-center gap-1 py-1 px-3"
+            >
+              Send
+            </HoverBorderGradient>
+          </div>
         </div>
 
       </div>
+
     </div>
   )
 }
